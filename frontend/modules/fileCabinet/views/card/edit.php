447d5d5443741349326336c7cb75af4079999c36
VlaVer98 <Verholantsev1998@gmail.com>
2020-07-28 12:30:22 +0700

    Renamed header in view card/edit In view card/edit of module fileCabinet, rename header.

--- frontend/modules/fileCabinet/views/card/edit.php
+++ frontend/modules/fileCabinet/views/card/edit.php
@@ -3,12 +3,11 @@
         <div class="col-md-12">
             <div class="box box-success">
                 <div class="box-header with-border">
-                    <h4>Новая заметка</h4>
+                    <h4>Редактирование карточки</h4>
                 </div>
                 <div class="box-body">
                     <?= $this->render('_createForm', [
                         'model' => $model,
-                        'card' => $card,
                         'tags' => $tags,
                         'cards' => $cards,
                     ]) ?>
